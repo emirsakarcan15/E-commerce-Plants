e9e5d25961ba29fe1ab29a9440c5986c01da768a
emirsakarcan15 <emirsakarcanau@gmail.com>
2025-05-17 14:27:34 +0300

    fixed deployment issues 3.0

--- plant/src/main.jsx
+++ plant/src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store} >
-        <BrowserRouter>
+        <BrowserRouter basename='/E-commerce-Plants' >
             <App />
         </BrowserRouter>
     </Provider>
